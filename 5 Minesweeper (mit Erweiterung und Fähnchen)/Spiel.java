e8c97222d0e1d2ee6b197d2e2cbb212f6af6d4ab
Chris_di_Ride <73610466+ASG-Reitmaier@users.noreply.github.com>
2025-06-19 23:07:44 +0200

    Add files via upload

---
+++ 5 Minesweeper (mit Erweiterung und Fähnchen)/Spiel.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Test here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Spiel
+{
+
+     public static void main(String[] args){
+         Model m = new Model(20,20);
+         Controller c = new Controller(m);
+    }
+
+}
